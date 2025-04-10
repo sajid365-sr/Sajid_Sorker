75c597d2ded213f8ede3d07c16e1c87867694fa8
sajid365-sr <sajidsorker86@gmail.com>
2025-04-10 09:06:38 +0600

    project updated

--- src/modules/Home/Home.tsx
+++ src/modules/Home/Home.tsx
@@ -1,26 +1,23 @@
-import React from 'react'
+import React from "react";
 //
-import About from './sections/About'
-import GetInTouch from './sections/GetInTouch'
-import Hero from './sections/Hero'
-import Projects from './sections/Projects'
-import Worked from './sections/Worked'
-import Footer from '@widgets/Footer'
-
+import About from "./sections/About";
+import GetInTouch from "./sections/GetInTouch";
+import Hero from "./sections/Hero";
+import Projects from "./sections/Projects";
+import Worked from "./sections/Worked";
+import Footer from "@widgets/Footer";
 
 const Home = () => {
   return (
     <>
-
       <Hero />
       <About />
-      {/* <Worked /> */}
+      <Worked />
       <Projects />
       <GetInTouch />
       <Footer />
-
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
